cd757874afe7c5b158417b656376e546ee91489a
Oskar Allerslev <oskar.m1660@gmail.com>
2025-01-18 14:12:17 +0100

    Add new routes and templates for integrals and normal distribution; update index and module1 templates for improved user experience

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/module1')
-def module1():
-    return render_template('module1.html')
+@app.route('/integraler')
+def integraler():
+    return render_template('integraler.html')
 
 @app.route('/definite-integral')
 def definite_integral():
@@ -17,6 +17,10 @@ def definite_integral():
 @app.route('/probability-intro')
 def probability_intro():
     return render_template('probability_intro.html')
+
+@app.route('/normalfordelingen')
+def normalfordelingen():
+    return render_template('normalfordelingen.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
